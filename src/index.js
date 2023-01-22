1bf65cf79a5133c9f87d8d66e4bc418c82d3447e
Tien <quangtien05022001@gmail.com>
2023-01-22 10:02:44 +0700

    day la comment cua tien

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const renderApp = () => {
     ReactDOM.render(
         <Provider store={reduxStore}>
             <IntlProviderWrapper>
-                <App persistor={persistor}/>
+                <App persistor={persistor} />
             </IntlProviderWrapper>
         </Provider>,
         document.getElementById('root')
@@ -26,4 +26,5 @@ renderApp();
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
+// day la comment cua Tien
 serviceWorker.unregister();
